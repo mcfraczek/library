c974a1c7343bf0f65af13a971a2855d665ee7eb7
mcfraczek <mcfraczek@gmail.con>
2018-11-07 13:43:33 +0100

    AddUser form working AddBook working without lists

---
+++ src/main/java/com/project/library/ab_helperBackingBeans/ListHelper.java
@@ -0,0 +1,27 @@
+package com.project.library.ab_helperBackingBeans;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ListHelper {
+    default List<String> list(String folder, String name) {
+        List<String> list = new ArrayList<>();
+        Path path = Paths.get("src", "main", "java", "com", "project", "library", "ab_helperBackingBeans");
+        Path folderName = Paths.get(folder, name);
+
+        try (InputStream inputStream = Files.newInputStream(path.resolve(folderName));
+             Reader reader = new InputStreamReader(inputStream);
+             BufferedReader br = new BufferedReader(reader)) {
+            br.lines().forEach(s -> {
+                list.add(s.trim());
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
--- src/main/java/com/project/library/ab_helperBackingBeans/book/BookPlus.java
+++ src/main/java/com/project/library/ab_helperBackingBeans/book/BookPlus.java
@@ -1,31 +1,16 @@
 package com.project.library.ab_helperBackingBeans.book;
 
 import com.project.library.a_entity.Book;
+import com.project.library.ab_helperBackingBeans.ListHelper;
 import lombok.Data;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class BookPlus {
+public class BookPlus implements ListHelper {
     private Book book;
 
-    public static List<String> genresList() {
-        List<String> genresList = new ArrayList<>();
-        Path path = Paths.get("src", "main", "java", "com", "project", "library", "ab_helperBackingBeans", "book", "bookGenres.txt");
-        try (InputStream inputStream = Files.newInputStream(path);
-             Reader reader = new InputStreamReader(inputStream);
-             BufferedReader br = new BufferedReader(reader)) {
-            br.lines().forEach(s -> {
-                genresList.add(s.trim());
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return genresList;
+    public List<String> genresList() {
+        return list("book", "bookGenres.txt");
     }
 }
