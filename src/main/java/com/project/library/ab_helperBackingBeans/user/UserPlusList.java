e09f5e7bf15c246e20aff9ae019fb23d34f503b5
mcfraczek <mcfraczek@gmail.con>
2018-11-07 13:56:56 +0100

    AddUser form working AddBook working without lists

--- src/main/java/com/project/library/ab_helperBackingBeans/user/UserPlusList.java
+++ src/main/java/com/project/library/ab_helperBackingBeans/user/UserPlusList.java
@@ -2,9 +2,11 @@ package com.project.library.ab_helperBackingBeans.user;
 
 import com.project.library.a_entity.User;
 import com.project.library.ab_helperBackingBeans.ListHelper;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class UserPlusList implements ListHelper {
     private User user;
 
--- src/main/java/com/project/library/d_controllers/AddingUserController.java
+++ src/main/java/com/project/library/d_controllers/AddingUserController.java
@@ -1,6 +1,6 @@
 package com.project.library.d_controllers;
 
-import com.project.library.a_entity.User;
+import com.project.library.ab_helperBackingBeans.user.UserPlusList;
 import com.project.library.c_service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,14 +15,14 @@ public class AddingUserController {
     private UserService userService;
 
     @RequestMapping("/addUser")
-    private String showForm(Model model, User user) {
+    private String showForm(Model model, UserPlusList user) {
         model.addAttribute("user", user);
         return "addUserForm";
     }
 
     @PostMapping("/processAddUserForm")
-    private String processForm(@ModelAttribute("user") User user) {
-        userService.saveUser(user);
+    private String processForm(@ModelAttribute("user") UserPlusList user) {
+        userService.saveUser(user.getUser());
         return "userSaved";
     }
 }
