a2cebda148e446abbc3a7c4080d0d76afe95352d
mcfraczek <mcfraczek@gmail.con>
2018-11-11 13:15:21 +0100

    Add User i Add Book działają bez buttonów usuwających w AddBook

--- src/main/resources/static/addBookForm/js/script.js
+++ src/main/resources/static/addBookForm/js/script.js
@@ -1,18 +1,18 @@
 var authorCount = 1;
 var divRowOpen = '<div class="form-row">';
+var button = ' <div class="col-md-2 d-flex justify-content-center align-items-end"><button type="button" class="btn btn-light btn-sm mb-1" id="deleteAuthor"><i class="fas fa-user-minus"></i></button></div>';
 
 function divColOpen(col) {
     return '<div class="col-md-' + col + '">';
 };
-var divColOpen6 = divColOpen(6);
 var divClose = "</div>";
 
 $("#addAuthor").on("click", function () {
-    var inputHtml = divRowOpen + divColOpen6 + '<label for="author' + authorCount + 'Name" class="text-light">Author\'s ' + (authorCount + 1) + ' Name/Names</label>' +
+    var inputHtml = divRowOpen + divColOpen(5) + '<label for="author' + authorCount + 'Name" class="text-light">Author\'s ' + (authorCount + 1) + ' Name/Names</label>' +
         '<input type="text" id="author' + authorCount + 'Name" placeholder="Alan Alexander" name="book.authorList[' + authorCount + '].name" class="form-control" />' +
-        divClose + divColOpen6 + '<label for="author' + authorCount + 'Surname" class="text-light">Author\'s ' + (authorCount + 1) + ' Surname</label>'
+        divClose + divColOpen(5) + '<label for="author' + authorCount + 'Surname" class="text-light">Author\'s ' + (authorCount + 1) + ' Surname</label>'
         + '<input type="text" id="author' + authorCount + 'Surname" placeholder="Milne" name="book.authorList[' + authorCount + '].surname" class="form-control"/>'
-        + divClose + divClose;
+        + divClose + button + divClose;
     authorCount++;
     $("#authors").append(inputHtml);
 });
@@ -27,3 +27,6 @@ $("#addGenre").on("click", function () {
     var newstr = str.replace(re, typesCount++);
     typesCopyTo.append(divRowOpen + newstr + divClose);
 });
+/*arase Authors*/
+
+var buttonDeleteAuthors = $("#deleteAuthor");
