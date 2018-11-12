2a5714e64904308c1f004cdd7b1eb6f1af168437
mcfraczek <mcfraczek@gmail.con>
2018-11-12 12:54:14 +0100

    Add User i Add Book działają bez buttonów usuwających w AddBook

--- src/main/java/com/project/library/ab_helperBackingBeans/ListHelper.java
+++ src/main/java/com/project/library/ab_helperBackingBeans/ListHelper.java
@@ -1,29 +1,25 @@
 package com.project.library.ab_helperBackingBeans;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public interface ListHelper {
     default List<String> list(String folder, String name) {
-        List<String> list = new ArrayList<>();
+        List<String> list = null;
         Path path = Paths.get("src", "main", "java", "com", "project", "library", "ab_helperBackingBeans");
         Path folderName = Paths.get(folder, name);
 
-        try (InputStream inputStream = Files.newInputStream(path.resolve(folderName));
-             Reader reader = new InputStreamReader(inputStream);
-             BufferedReader br = new BufferedReader(reader)) {
-            br.lines().forEach(s -> {
-                list.add(s.trim());
-            });
+        try {
+            list = Files.readAllLines(path.resolve(folderName));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return list;
     }
+
 
     List<String> listFromDirectFile();
 }
