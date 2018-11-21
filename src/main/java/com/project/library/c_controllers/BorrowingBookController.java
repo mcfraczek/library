eab375775bf832e0651a5008da674b5eb9d2640d
mcfraczek <mcfraczek@gmail.con>
2018-11-21 21:35:36 +0100

    Fully Functional Finding Of User

--- src/main/java/com/project/library/c_controllers/BorrowingBookController.java
+++ src/main/java/com/project/library/c_controllers/BorrowingBookController.java
@@ -1,6 +1,8 @@
 package com.project.library.c_controllers;
 
+import com.project.library.a_entity.Book;
 import com.project.library.a_entity.User;
+import com.project.library.b_DAO.UserDAO;
 import com.project.library.b_a_service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BorrowingBookController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserDAO userDAO;
 
     @RequestMapping("/borrowingBookForm")
 
@@ -31,8 +36,11 @@ public class BorrowingBookController {
     }
 
     @RequestMapping("/borrowingBookDetails")
-    public String borrowingBookDetails(@RequestParam("userId") int userId) {
+    public String borrowingBookDetails(@RequestParam("userId") int userId, Model model) {
         /*show user By id*/
+        Optional<User> user = userDAO.findById(userId);
+        List<Book> booksBorrowedByUser = user.get().getBookList();
+        model.addAttribute("books", booksBorrowedByUser);
         return "borrowingBookDetails";
     }
 }
