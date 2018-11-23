24dac4a5d6be1e6694747d47930ef3dd788c68c5
mcfraczek <mcfraczek@gmail.con>
2018-11-23 12:08:13 +0100

    Fully Functional Finding Of User

--- src/main/java/com/project/library/b_a_service/AuthorService.java
+++ src/main/java/com/project/library/b_a_service/AuthorService.java
@@ -1,21 +1,63 @@
 package com.project.library.b_a_service;
 
 import com.project.library.a_entity.Author;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class AuthorService {
     @Autowired
     private EntityManager entityManager;
 
-    public List<Author> findByAuthor(String authorSurname) {
-        List<Author> authorList = entityManager.createQuery
-                ("from Author a where a.surname=" + "'" + authorSurname + "'", Author.class)
-                .getResultList();
+    public List<Author> findByAuthor(String authorsNS) {
+        List<AuthorHelper> authorHelperList = fromStringToSurnameName(authorsNS);
+        List<Author> authorList = new ArrayList<>();
+
+        for (AuthorHelper authorHelper : authorHelperList) {
+            List<Author> helper = entityManager.createQuery
+                    ("from Author a where a.name=" + "'" + authorHelper.getName() + "'" +
+                            " and a.surname=" + "'" + authorHelper.getSurname() + "'", Author.class)
+                    .getResultList();
+            authorList.addAll(helper);
+        }
+        if (authorHelperList.size() > 1) { /* jeśli jest wiecej niż jeden autor*/
+                /*jeśli lista książek autora nie równa się ządnej liście książek żadenego autora.
+                Usuń tego autora */
+        }
+
         return authorList;
     }
+
+    private List<AuthorHelper> fromStringToSurnameName(String authorsSN) {
+        Pattern pattern = Pattern.compile("\\w+\\s\\w+");
+        Matcher matcher = pattern.matcher(authorsSN);
+        List<String> authorsList = new ArrayList<>();
+        while (matcher.find()) {
+            authorsList.add(matcher.group(0));
+        }
+
+        List<AuthorHelper> authorHelperList = new ArrayList<>();
+        for (String s : authorsList) {
+            String[] surnameName = s.split("\\s");
+            authorHelperList.add(new AuthorHelper(surnameName[0], surnameName[1]));
+        }
+        return authorHelperList;
+    }
+}
+
+@ToString
+@AllArgsConstructor
+@Data
+class AuthorHelper {
+    private String name;
+    private String surname;
 }
--- src/main/java/com/project/library/b_a_service/BookService.java
+++ src/main/java/com/project/library/b_a_service/BookService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
@@ -17,23 +19,36 @@ public class BookService {
     private EntityManager entityManager;
 
     @Transactional
-    public List<Book> find(String title, String author, String libraryNumber) {
+    public List<Book> find(String title, String authorNS, String libraryNumber) {
         List<Book> bookList = null;
 
-        if (author.isEmpty() && libraryNumber.isEmpty()) {
+        if (authorNS.isEmpty() && libraryNumber.isEmpty()) {
             bookList = findByTitle(title);
-        } else if (author.isEmpty()) {
+        } else if (libraryNumber.isEmpty() && title.isEmpty()) {
+            List<Author> authorList = authorService.findByAuthor(authorNS);
+            bookList = new ArrayList<>();
+            for (Author author : authorList) {
+                List<Book> bookList1 = author.getBookList();
+                for (Book book : bookList1) {
+                    bookList.add(book);
+                }
+            }
+        } else if (title.isEmpty() && authorNS.isEmpty()) {
+            bookList = entityManager.createQuery("from Book b where b.libraryNumber=" + "'" + libraryNumber + "'"
+                    , Book.class).getResultList();
+        } else if (authorNS.isEmpty()) {
             bookList = entityManager.createQuery("from Book b where b.title=" + "'" + title + "'" + "" +
                     " and b.libraryNumber=" + "'" + libraryNumber + "'", Book.class).getResultList();
         } else if (libraryNumber.isEmpty()) {
             List<Book> bookList1 = entityManager.createQuery("from Book b where b.title=" + "'" + title + "'"
                     , Book.class).getResultList();
-            bookList = findByAuthor(author, bookList1);
+
+            bookList = findByAuthor(authorNS, bookList1);
         } else {
             bookList = entityManager.createQuery("from Book b where b.title=" + "'" + title + "'" + "" +
                     " and b.libraryNumber=" + "'" + libraryNumber + "'", Book.class).getResultList();
         }
-        return bookList;
+        return bookList.stream().distinct().collect(Collectors.toList());
     }
 
     private List<Book> findByTitle(String title) {
@@ -44,16 +59,15 @@ public class BookService {
         return bookList;
     }
 
-    private List<Book> findByAuthor(String author, List<Book> bookList) {
-        List<Author> authorList = authorService.findByAuthor(author);
-        List<Author> authorListFromBook = null;
-        for (Book book : bookList) {
-            authorListFromBook = book.getAuthorList();
-        }
-        System.out.println(authorList.equals(authorListFromBook));
-        List<Book> bookList2 = null;
-        /*filtr nie wiem jak*/
-        return bookList;
+    /*jeśli któraś z list autorów z listy książek będzie się zgadzała - zostaw te książki*/
+    private List<Book> findByAuthor(String authorSurname, List<Book> bookList) {
+        List<Author> authorList = authorService.findByAuthor(authorSurname);
+
+        List<Book> finalList = bookList.stream()
+                .filter(b -> b.getAuthorList().equals(authorList))
+                .collect(Collectors.toList());
+        System.out.println(finalList);
+        return finalList;
     }
 
     private List<Book> findByLibraryNumber(String libraryNumber) {
