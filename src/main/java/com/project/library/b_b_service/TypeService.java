9eee3085fb9083698173c171a0f8f93a1dacc71e
mcfraczek <mcfraczek@gmail.con>
2018-11-26 20:02:23 +0100

    :)

---
+++ src/main/java/com/project/library/b_b_service/TypeService.java
@@ -0,0 +1,18 @@
+package com.project.library.b_b_service;
+
+import com.project.library.a_entity.Type;
+import com.project.library.b_a_DAO.TypeDEO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TypeService {
+    @Autowired
+    private TypeDEO typeDEO;
+
+    public Optional<Type> findTypeByType(String type) {
+        return typeDEO.findTypeByType(type);
+    }
+}
