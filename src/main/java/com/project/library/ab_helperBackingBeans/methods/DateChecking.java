3eb11f7884a17f1552c750c8b5b55a3831f24181
mcfraczek <mcfraczek@gmail.con>
2018-11-27 20:20:16 +0100

    Problem z javaScriptem w addBook

--- src/main/java/com/project/library/ab_helperBackingBeans/methods/DateChecking.java
+++ src/main/java/com/project/library/ab_helperBackingBeans/methods/DateChecking.java
@@ -1,7 +1,10 @@
 package com.project.library.ab_helperBackingBeans.methods;
 
+import com.project.library.a_entity.Book;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public interface DateChecking {
     static boolean thereIsAFine(String date) {
@@ -12,6 +15,18 @@ public interface DateChecking {
         }
         return false;
     }
+
+    static boolean thereIsAFine(List<Book> bookList) {
+        for (Book book : bookList) {
+            LocalDate timeBorrowed = LocalDate.from(DateTimeFormatter.ISO_LOCAL_DATE.parse(book.getDate()));
+            LocalDate nowMinus30 = LocalDate.now();
+            if (nowMinus30.equals(timeBorrowed)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
 /*  static boolean thereIsAFine(String date) {
         LocalDate timeBorrowed = LocalDate.from(DateTimeFormatter.ISO_LOCAL_DATE.parse(date));
