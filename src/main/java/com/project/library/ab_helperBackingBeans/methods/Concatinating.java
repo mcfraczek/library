f567dc9d3b57ca34da6c1254bbee95f446f905f3
mcfraczek <mcfraczek@gmail.con>
2018-11-27 16:57:47 +0100

    Problem z javaScriptem w addBook

--- src/main/java/com/project/library/ab_helperBackingBeans/methods/Concatinating.java
+++ src/main/java/com/project/library/ab_helperBackingBeans/methods/Concatinating.java
@@ -5,8 +5,8 @@ import com.project.library.a_entity.Author;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Concatinating {
-    public static String concatinate(List<Author> authorList) {
+public interface Concatinating {
+    static String concatinate(List<Author> authorList) {
         return authorList.stream().map(author -> author.getName() + " " + author.getSurname())
                 .collect(Collectors.joining(", "));
     }
--- src/main/java/com/project/library/ab_helperBackingBeans/methods/DateChecking.java
+++ src/main/java/com/project/library/ab_helperBackingBeans/methods/DateChecking.java
@@ -3,8 +3,8 @@ package com.project.library.ab_helperBackingBeans.methods;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class DateChecking {
-    public static boolean thereIsAFine(String date) {
+public interface DateChecking {
+    static boolean thereIsAFine(String date) {
         LocalDate timeBorrowed = LocalDate.from(DateTimeFormatter.ISO_LOCAL_DATE.parse(date));
         LocalDate nowMinus30 = LocalDate.now().minusDays(30);
         if (nowMinus30.isAfter(timeBorrowed)) {
