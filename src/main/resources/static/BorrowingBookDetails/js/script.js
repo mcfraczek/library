4233bfd9091e025841b107d0d5700b7f88f022c1
mcfraczek <mcfraczek@gmail.con>
2018-11-27 15:20:56 +0100

    Problem z javaScriptem w addBook

--- src/main/resources/static/BorrowingBookDetails/js/script.js
+++ src/main/resources/static/BorrowingBookDetails/js/script.js
@@ -1,11 +1,16 @@
 $("#back").on("click", function () {
     window.history.back();
 });
-var dateFromHtml = Date.parse(document.getElementById("date").innerText);
-var dateNow = new Date();
-/*converting ms to day*/
-var day = 3600000 * 24;
-var numberOfDays = Math.floor((dateNow - dateFromHtml) / day);
-if (numberOfDays > 1) {
-    $("#date").addClass("red");
+dateToRed();
+
+function dateToRed() {
+    var dateFromHtml = Date.parse(document.getElementById("date").innerText);
+    var dateNow = new Date();
+    /*converting ms to day*/
+    var day = 3600000 * 24;
+    var numberOfDays = Math.floor((dateNow - dateFromHtml) / day);
+    if (numberOfDays > 1) {
+        $("#date").addClass("red");
+    }
 }
+
