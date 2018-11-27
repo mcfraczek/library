46d05c59ce366804a77573f0c1392a95c460372d
mcfraczek <mcfraczek@gmail.con>
2018-11-27 13:51:30 +0100

    Problem z javaScriptem w addBook

--- src/main/java/com/project/library/a_entity/Type.java
+++ src/main/java/com/project/library/a_entity/Type.java
@@ -17,4 +17,14 @@ public class Type {
     @JoinTable(name = "book_type", joinColumns = @JoinColumn(name = "type_id"),
             inverseJoinColumns = @JoinColumn(name = "book_id"))
     private List<Book> bookList;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Type type1 = (Type) o;
+
+        return type != null ? type.equals(type1.type) : type1.type == null;
+    }
 }
