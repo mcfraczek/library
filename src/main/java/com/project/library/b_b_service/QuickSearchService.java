f3c0e3c63a0fe7305a53d82c56a49599fd8a80a2
mcfraczek <mcfraczek@gmail.con>
2018-11-29 16:33:29 +0100

    Problem z javaScriptem w addBook

---
+++ src/main/java/com/project/library/b_b_service/QuickSearchService.java
@@ -0,0 +1,13 @@
+package com.project.library.b_b_service;
+
+import com.project.library.a_entity.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuickSearchService {
+    public List<Book> search(String searched) {
+        return null;
+    }
+}
