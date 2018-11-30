97ee186b4ca7cf24076e2b385227bf1100d7dcc0
mcfraczek <mcfraczek@gmail.con>
2018-11-30 17:49:11 +0100

    Problem z javaScriptem w addBook

--- src/main/java/com/project/library/b_a_DAO/AuthorDAO.java
+++ src/main/java/com/project/library/b_a_DAO/AuthorDAO.java
@@ -1,6 +1,7 @@
 package com.project.library.b_a_DAO;
 
 import com.project.library.a_entity.Author;
+import com.project.library.a_entity.Book;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -12,4 +13,6 @@ public interface AuthorDAO extends CrudRepository<Author, Integer> {
     List<Author> findAuthorsByNameIgnoreCase(String name);
 
     List<Author> findAuthorsBySurnameIgnoreCase(String surname);
+
+    List<Author> findAuthorsByBookListContaining(Book book);
 }
