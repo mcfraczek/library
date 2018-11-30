a7d526ea9a0679fa9dc2d1751a73b0dbe8b7051b
mcfraczek <mcfraczek@gmail.con>
2018-11-30 19:32:34 +0100

    Problem z javaScriptem w addBook

--- src/main/java/com/project/library/c_controllers/AddingUserController.java
+++ src/main/java/com/project/library/c_controllers/AddingUserController.java
@@ -3,11 +3,8 @@ package com.project.library.c_controllers;
 import com.project.library.ab_helperBackingBeans.user.UserPlusList;
 import com.project.library.b_b_service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +14,6 @@ public class AddingUserController {
 
     @Autowired
     private UserService userService;
-
-    @InitBinder
-    public void initBinder(WebDataBinder webDataBinder) {
-        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
-        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
-    }
 
     @RequestMapping("/addUser")
     private String showForm(Model model, UserPlusList user) {
--- src/main/java/com/project/library/c_controllers/SearchingForBookController.java
+++ src/main/java/com/project/library/c_controllers/SearchingForBookController.java
@@ -4,11 +4,8 @@ import com.project.library.a_entity.Book;
 import com.project.library.ab_helperBackingBeans.book.BookPlusList;
 import com.project.library.b_b_service.BookService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,12 +16,6 @@ import java.util.List;
 @Controller
 public class SearchingForBookController {
     private BookService bookService;
-
-    @InitBinder
-    public void initBinder(WebDataBinder webDataBinder) {
-        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
-        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
-    }
 
     @RequestMapping("/searchForABook")
     public ModelAndView showSearch() {
