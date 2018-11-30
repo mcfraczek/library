0d088991cd770d095701a3370f103074de9c8c05
mcfraczek <mcfraczek@gmail.con>
2018-11-30 15:01:00 +0100

    Problem z javaScriptem w addBook

--- src/main/resources/static/BorrowingBookDetails/js/script.js
+++ src/main/resources/static/BorrowingBookDetails/js/script.js
@@ -17,7 +17,7 @@ function dateToRed() {
         /*5zł za dzień*/
         var fine = numberOfDays * 5;
         sumOfFine += fine;
-        if (numberOfDays > 7) {
+        if (numberOfDays > 2) {
             array[i].parentElement.classList.add("red");
         }
     }
