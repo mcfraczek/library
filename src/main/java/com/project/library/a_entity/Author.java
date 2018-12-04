f132c146e76e58095debf6f298490ba3cdb8b557
mcfraczek <mcfraczek@gmail.con>
2018-12-04 13:58:00 +0100

    Problem z javaScriptem w addBook

--- src/main/java/com/project/library/a_entity/Author.java
+++ src/main/java/com/project/library/a_entity/Author.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -14,8 +16,12 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotNull
+    @Size(max = 45, message = "Author's name must not exceed 45 characters")
     @Column(name = "name")
     private String name;
+    @NotNull
+    @Size(max = 45, message = "Author's surname must not exceed 45 characters")
     @Column(name = "surname")
     private String surname;
     @ManyToMany(fetch = FetchType.EAGER,
