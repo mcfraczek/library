3ffff2f747997a2dc73120c58453217ced2e6872
mcfraczek <mcfraczek@gmail.con>
2018-12-04 12:48:33 +0100

    Problem z javaScriptem w addBook

--- src/main/java/com/project/library/c_controllers/AddingBookController.java
+++ src/main/java/com/project/library/c_controllers/AddingBookController.java
@@ -3,15 +3,27 @@ package com.project.library.c_controllers;
 import com.project.library.ab_helperBackingBeans.book.BookPlusList;
 import com.project.library.b_b_service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class AddingBookController {
     @Autowired
     private BookService bookService;
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/addBook")
     public ModelAndView showForm(BookPlusList book) {
@@ -19,7 +31,11 @@ public class AddingBookController {
     }
 
     @RequestMapping("/processAddBookForm")
-    public String processForm(@ModelAttribute("book") BookPlusList bookPlusList) {
+    public String processForm(@Valid @ModelAttribute("book") BookPlusList bookPlusList,
+                              BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "addBookForm";
+        }
         bookService.saveBook(bookPlusList.getBook());
         return "addBookForm";
     }
