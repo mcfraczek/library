6c2c50072239276f1b4af93e0da23b8c160f5370
mcfraczek <mcfraczek@gmail.con>
2018-12-04 12:48:33 +0100

    Problem z javaScriptem w addBook

--- src/main/java/com/project/library/a_entity/Book.java
+++ src/main/java/com/project/library/a_entity/Book.java
@@ -3,8 +3,12 @@ package com.project.library.a_entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
+/*@NotNull(message = "is required")
+    @Size(min = 4, message = "is required")*/
 @Data
 @Entity
 @Table(name = "book")
@@ -12,10 +16,16 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotNull
+    @Size(max = 60)
     @Column(name = "title")
     private String title;
+    @NotNull
+    @Size(min = 1)
     @Column(name = "isbn")
     private String ISBN;/*restrykcja*/
+    @NotNull
+    @Size(min = 1)
     @Column(name = "library_number")
     private String libraryNumber;
     @Column(name = "time_stamp")
@@ -24,6 +34,7 @@ public class Book {
     @JoinTable(name = "book_type", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "type_id"))
     private List<Type> typeList;
+    @NotNull
     @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
     @JoinTable(name = "book_author",
             joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
--- src/main/java/com/project/library/ab_helperBackingBeans/book/BookPlusList.java
+++ src/main/java/com/project/library/ab_helperBackingBeans/book/BookPlusList.java
@@ -4,15 +4,16 @@ import com.project.library.a_entity.Book;
 import com.project.library.ab_helperBackingBeans.ListHelper;
 import lombok.Data;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Data
 public class BookPlusList implements ListHelper {
+    @Valid
     private Book book;
 
     @Override
     public List<String> listFromDirectFile() {
         return list("book", "bookGenres.txt");
     }
-
 }
