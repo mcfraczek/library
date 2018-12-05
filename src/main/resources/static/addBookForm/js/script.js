2e3086dab306719c15af2df8d23e37cb81d8e987
mcfraczek <mcfraczek@gmail.con>
2018-12-05 07:31:51 +0100

    :)

--- src/main/resources/static/addBookForm/js/script.js
+++ src/main/resources/static/addBookForm/js/script.js
@@ -1,14 +1,16 @@
 var authorCount = 1;
+var typesCount = 1;
 var divRowOpenClass = '<div class="form-row mt-3">';
 var divRowOpenClass2 = '<div class="form-row w-100 px-0 mb-3">';
 var divColOpenClass12 = '<div class="col-md-12 px-0">';
-var button = ' <div class="col-md-2 d-flex justify-content-center align-items-end"><button type="button" class="btn btn-light btn-sm mb-1 deleteAuthor" ><i class="fas fa-user-minus"></i></button></div>';
+var button = ' <div class="col-md-2"></div>';
 
 function divColOpen(col) {
     return '<div class="col-md-' + col + '">';
 };
 var divClose = "</div>";
 
+/*Add Author*/
 $("#addAuthor").on("click", function () {
     var inputHtml = divRowOpenClass + divColOpen(5) + '<label for="author' + authorCount + 'Name" class="text-light">Author\'s ' + (authorCount + 1) + ' Name/Names</label>' +
         '<input type="text" id="author' + authorCount + 'Name" placeholder="Alan Alexander" name="book.authorList[' + authorCount + '].name" class="form-control" required maxlength="45"/>' +
@@ -18,24 +20,31 @@ $("#addAuthor").on("click", function () {
     authorCount++;
     $("#authors").append(inputHtml);
 });
-
+/*erase Authors*/
+$("#eraseAuthor").on("click", function () {
+    $("#authors .form-row:nth-of-type(" + (authorCount - 1) + ")").remove();
+    authorCount--;
+    if (authorCount <= 0) {
+        authorCount = 1;
+    }
+});
 /*Adding genre*/
 var typesFrom = $("#copyFrom").html();
 var typesCopyTo = $("#types");
-var typesCount = 1;
 $("#addGenre").on("click", function () {
     var str = typesFrom;
     var re = /([\d+])/g;
     var newstr = str.replace(re, typesCount++);
     typesCopyTo.append(divRowOpenClass2 + divColOpenClass12 + newstr + divClose + divClose);
 });
-/*erase Authors*/
-
-$("#authors").on("click", ".deleteAuthor", function () {
-    authorCount--;
-    $(this).parent().parent().remove();
+/*removing genre*/
+$("#eraseGenre").on("click", function () {
+    $("#types .form-row:nth-of-type(" + (typesCount - 1) + ")").remove();
+    typesCount--;
+    if (typesCount <= 0) {
+        typesCount = 1;
+    }
 });
-
 /*erase button*/
 $("#error").on("click", function () {
     $(this).fadeOut(500, function () {
@@ -51,16 +60,13 @@ $('input[type="submit"]').on("click", function () {
     }
     alert(html);
 });*/
-/*$("#submit").on("click", function () {
-    alert($("input"));
+/*  var html;
+  for (var i = 0; i < authorCount; i++) {
+      html = $("#authors .form-row:eq(" + i + ")").html();
+      $("#authors .form-row:eq(" + i + ")").html(change2(html, i + 1));
+  }*/
 
-    var html;
-    for (var i = 0; i < authorCount; i++) {
-        html = $("#authors .form-row:eq(" + i + ")").html();
-        $("#authors .form-row:eq(" + i + ")").html(change2(html, i + 1));
-    }
-});
-function change2(input, number) {
+/*function change2(input, number) {
     var reg = /\[(\d+)\]/g;
     return input.replace(reg, '[' + number + ']');
 }*/
