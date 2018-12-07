5250886856f232e73f56c2941a97f4b0c28fb2b0
mcfraczek <mcfraczek@gmail.con>
2018-12-07 13:25:34 +0100

    :)

--- src/main/java/com/project/library/ab_helperBackingBeans/methods/ForceDeleteUser.java
+++ src/main/java/com/project/library/ab_helperBackingBeans/methods/ForceDeleteUser.java
@@ -1,16 +1,12 @@
 package com.project.library.ab_helperBackingBeans.methods;
 
-import com.project.library.b_b_service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class ForceDeleteUser {
-    @Autowired
-    private UserService userService;
 
-    public static boolean thisUserHasBooks(int id1, int id2) {
+    public static boolean hasError(int id1, int id2) {
         if (id1 == id2) {
             return true;
         }
